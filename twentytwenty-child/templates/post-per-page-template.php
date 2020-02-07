ea14687d2e9c9814e58d00cc5898e77c011e788f
IcemanHHW <22617@ma-web.nl>
2020-02-07 17:34:45 +0100

    Homework week 2
    Added Nopaging query and posts per page query template

---
+++ twentytwenty-child/templates/post-per-page-template.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Posts per page template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+
+<?php
+
+$args = array(  
+    'post_type' => 'page',   
+    'posts_per_page' => -1,
+);
+
+
+$loop = new WP_Query( $args ); 
+    
+while ( $loop->have_posts() ) : $loop->the_post(); 
+?>
+    <h3><?php the_title(); ?></h3> 
+    <p><?php the_excerpt(); ?> </p>
+<?php
+endwhile;
+
+wp_reset_postdata(); 
+?>
+
+
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php get_footer(); ?>
